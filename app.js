db9b259b7de448c0b76fe0a9abbdb666660fc851
Htet Oo Wai Yan <140035139+sc3p73r-it@users.noreply.github.com>
2025-07-14 12:53:46 +0630

    setup nodejs workflow

--- app.js
+++ app.js
@@ -6,6 +6,10 @@ app.get('/', (req, res) => {
   res.send('Hello, Docker!');
 });
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+if (require.main === module) {
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
+}
+
+module.exports = app;
